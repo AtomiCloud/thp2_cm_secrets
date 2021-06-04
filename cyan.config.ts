79286be1aef12f2238c25db22ebf9f1eda5d0c4b
kirinnee <ernest@eng.atomi.cloud>
2021-06-04 15:11:07 +0800

    feat: initial commit

---
+++ cyan.config.ts
@@ -0,0 +1,17 @@
+import {Chalk} from "chalk";
+import {Inquirer} from "inquirer";
+import {Cyan, IAutoInquire, IAutoMapper, IExecute,} from "./Typings";
+
+export = async function (folderName: string, chalk: Chalk, inquirer: Inquirer, autoInquirer: IAutoInquire, autoMap: IAutoMapper, execute: IExecute): Promise<Cyan> {
+	
+	const question = {
+		name: ['ern-ng', 'Your Name (DNS compatible)']
+	}
+	
+	const answers = await autoInquirer.InquireInput(question);
+	
+	return {
+		globs: {root: "./Template", pattern: "**/*.*", ignore: ""},
+		variable: answers,
+	} as Cyan;
+}
